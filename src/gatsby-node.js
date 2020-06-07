97d7dfcfdda281547e06c8dc9725b883dd257db6
wonism <yocee57@gmail.com>
2020-06-08 04:16:46 +0900

    🐛 filter invalid pages like empty page

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -26,25 +26,27 @@ const getSources = async ({ actions: { createNode }, createContentDigest }, opti
     )
   );
 
-  pages.forEach(({ id, page: { type, titleString: description = '', title = '', content = '', resource = '' } }) => {
-    const page = title + content + resource;
+  pages
+    .filter(({ page: { title, titleString, content } }) => Boolean(title) && Boolean(titleString) && Boolean(content))
+    .forEach(({ id, page: { type, titleString: description, title, content, resource = '' } }) => {
+      const page = title + content + resource;
 
-    const node = {
-      id,
-      parent: null,
-      children: [],
-      contentType: type,
-      internal: {
-        type: 'NotionContent',
-        mediaType: 'text/html',
-        content: page,
-        contentDigest: createContentDigest(page),
-        description,
-      },
-    };
+      const node = {
+        id,
+        parent: null,
+        children: [],
+        contentType: type,
+        internal: {
+          type: 'NotionContent',
+          mediaType: 'text/html',
+          content: page,
+          contentDigest: createContentDigest(page),
+          description,
+        },
+      };
 
-    createNode(node);
-  });
+      createNode(node);
+    });
 };
 
 exports.sourceNodes = getSources;
